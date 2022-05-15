641153777e695c30f40fdea82f8ef20fab6d3693
ZaZa2006 <nurmatdovranov@gmail.com>
2022-05-16 00:11:11 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Class nomer = new Class(31);
+    Class soz = new Class("Kyrgyzstan");
+    Class massiv = new Class(new int[]{2005, 2010, 2020});
+
+    }
+}
